880b4ef5819655c4b6802246f5cb08fd0a8e28cf
Haoran Qiu <haoranq4@illinois.edu>
2024-05-24 22:31:48 -0500

    added more example commands

--- output-token-len-prediction/script.sh
+++ output-token-len-prediction/script.sh
@@ -1,13 +1,27 @@
-# python preprocess_dataset.py --task_type 2
-# python latency_prediction.py --task_type 2
-# python preprocess_dataset.py --task_type 1
-# python latency_prediction.py --task_type 1
-# python preprocess_dataset.py --task_type 0
-# python latency_prediction.py --task_type 0
-# python latency_prediction.py --task_type 0 --l1_loss
-# python latency_prediction.py --task_type 3
-# python latency_prediction.py --task_type 3 --l1_loss
+# single-round conversations with LLM
+# multi-class
+python preprocess_dataset.py --task_type 2 --data_size 10
+python latency_prediction.py --task_type 2 --data_size 10
+python latency_prediction.py --task_type 3 --data_size 10
+python latency_prediction.py --task_type 3 --l1_loss --data_size 10
 
+# binary-class
+python preprocess_dataset.py --task_type 1 --data_size 10
+python latency_prediction.py --task_type 1 --data_size 10
+
+# regression
+python preprocess_dataset.py --task_type 0 --data_size 10
+python latency_prediction.py --task_type 0 --data_size 10
+python latency_prediction.py --task_type 0 --l1_loss --data_size 10
+
+# multi-class
+python preprocess_dataset.py --task_type 2 --data_size 10 --all_models
+python latency_prediction.py --task_type 2 --data_size 10 --all_models
+# regression
+python preprocess_dataset.py --task_type 0 --data_size 10 --model_name "gpt-4"
+python latency_prediction.py --task_type 0 --data_size 10  --model_name "gpt-4"
+
+# multi-round conversations with LLM
 # multi-class
 # python preprocess_dataset.py --task_type 2 --multi_round
 # python latency_prediction.py --task_type 2 --multi_round
@@ -19,6 +33,6 @@
 # python latency_prediction.py --task_type 1 --multi_round
 
 # regression
-python preprocess_dataset.py --task_type 0 --multi_round
-python latency_prediction.py --task_type 0 --multi_round
-python latency_prediction.py --task_type 0 --multi_round --l1_loss
+# python preprocess_dataset.py --task_type 0 --multi_round
+# python latency_prediction.py --task_type 0 --multi_round
+# python latency_prediction.py --task_type 0 --multi_round --l1_loss
